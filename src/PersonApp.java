12087e206bddd0d048211cc13d272721d2cdacc5
dirasanjayawardana <dirawardana.dw@gmail.com>
2023-12-17 22:09:51 +0700

    backup

---
+++ src/PersonApp.java
@@ -0,0 +1,14 @@
+public class PersonApp {
+    public static void main(String[] srgs){
+        //------ cara membuat object person dari class Person ------//
+        var person1 = new Person("Dira", "Lampung");
+        Person person2 = new Person("Andi");
+        Person person3 = new Person();
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+        person1.sayHello("Udin");
+    }
+}
