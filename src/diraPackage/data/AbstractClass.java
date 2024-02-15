0ec5ae861ff3c5caca850e03901aeea8aa46fc2e
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-02-15 16:55:18 +0700

    Dao

--- src/diraPackage/data/AbstractClass.java
+++ src/diraPackage/data/AbstractClass.java
@@ -1,6 +1,7 @@
 package diraPackage.data;
 
 // Abstract class tidak bisa dibuat objek, tetapi hanya bisa diturunkan
+// hanya untuk kerangka saja
 public abstract class AbstractClass {
     public String name;
 }
